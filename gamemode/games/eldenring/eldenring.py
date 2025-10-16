881f889cb91214df2e1cf6fb3061bf022e64b06b
Lia Hepler Mackey <liaheplermackey@gmail.com>
2025-10-15 21:06:18 -0700

    Finished first draft of the eldenring setup

--- gamemode/games/eldenring/eldenring.py
+++ gamemode/games/eldenring/eldenring.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from talon import Module,Context,actions,cron
 
 ctx = Context()
@@ -46,31 +47,137 @@ I think forward and backward direction should automatically cancel out horizonta
 
 I think sometimes the directional buttons double fire in quick succession causing other inputs to be canceled out
 To fix this I may have to throttle inputs on the four directional+o
+
+To do stance ash of wars I need to make a flow state, have multiple configurations that flow into each other
+tut activates ash of war, then tut can deactivate it or click or oo will do an attack and release the button at the same time 
+I could simplify this by always releasing the ash of war button when attacks are inputted. Then I could not have to switch configurations constantly
+
+LARGE ISSUE: button inputs are being dropped like crazy I suspect that they are too quick for eldenring to pick up
+Additionally due to the non locked perspective, It is very very difficult in order to tell what direction I need input
+For example if I am circling an enemy then the back direction is changing constantly 
+This may be a challenge of execution, not a change necessary in the system
+
 """
 
-parrot_config = {
+
+def paired_command_wrapper(first: Callable, second: Callable):
+    first()
+    second()
+
+actions.user.set_global_variable("stance_active", False)
+
+def state_machine_mode_switcher(mode: str):
+    """This function is a mode switcher that enforces a certain flow to the mode switching so that 
+    only certain modes are accessible from others. The permitted switches are as follows:
+    default -> item, menu, stance
+    stance ->   item, menu, default 
+    item -> stance, default (depending on variables)
+    menu -> stance, default (depending on variables)
+    If input is "toggle" Then it will switch between stance and default
+    """
+    current_mode = actions.user.parrot_config_get_mode()
+    match mode:
+        case "default":
+            actions.user.parrot_config_set_mode("default")
+            actions.user.set_global_variable("stance_active", False)
+        case "stance":
+            actions.user.parrot_config_set_mode("stance")
+            actions.user.set_global_variable("stance_active", True)
+        case "item":
+            if current_mode == "default" or current_mode == "stance":
+                actions.user.parrot_config_set_mode("item")
+        case "menu":
+            if current_mode == "default" or current_mode == "stance":
+                actions.user.parrot_config_set_mode("menu")
+        case "toggle":
+            if current_mode == "default":
+                actions.user.parrot_config_set_mode("stance")
+                actions.user.set_global_variable("stance_active", True)
+            elif current_mode == "stance":
+                actions.user.parrot_config_set_mode("default")
+                actions.user.set_global_variable("stance_active", False)
+        case _:
+            print("Mode name not recognized for game eldenring")
+                
+    
+
+default_config = {
     "aa:th_150": ('move left' , lambda : actions.user.movement_button_down("left")),
     "oh:th_150": ('move right', lambda : actions.user.movement_button_down("right")),
     "ee:th_100": ('move up'   , lambda : actions.user.movement_button_down("up")),
     "er:th_150": ('move down' , lambda : actions.user.movement_button_down("down")),
     "eh": ('stop moving', lambda : actions.user.game_stop()),
-    "hiss": (),
-    "hiss_stop:db_250": (),
-    "shush": (),    
-    "shush_stop:db_250": (),
     "palate_click": ('light attack', lambda : actions.user.mouse_button(0, 16000)),
     "oo": ('heavy attack', lambda : actions.user.mouse_button_down(2)),  
     "oo_stop": ('heavy attack release', lambda : actions.user.mouse_button_up(2)),
-    "tut": ('ash of war', lambda : actions.user.button("p")),
+    "clock": ('ash of war', lambda : actions.user.button("p")),
 
-    "clock": (),
-    # "clock clock": ('heavy attack charged', lambda : actions.user.mouse_button_hold(2, )),
     "buzz:th_250": ('interact', lambda : actions.user.button("e")),
-    "mm:th_400": (lambda : actions.user.button("q")),
-    # "t": ('tester', lambda : print("tut")),
-    # "t aa": ('tester', lambda : print("aa")),
-    # "t ee": ('tester', lambda : print("ee")),
-    # "t oh": ('tester', lambda : print("oh")),
+    "mm:th_400": ('lock on', lambda : actions.user.button("q")),
+    "zh": ('jump', lambda : actions.user.button("f")),
+    "high_whistle:th_500": ('escape', lambda : actions.user.button("escape")),
+    "hiss": ('dodge', lambda : actions.user.button("l")),
+    "shush": ('use item', lambda : actions.user.button("r")),
+    "ll": (),
+    "tut": (),
+    "alveolar_click": (),
+}
+
+# Stance based ashes of war behave differently from others and therefore require a slightly different button layout
+stance_actions = {
+    "clock": ('ash of war toggle', lambda : actions.user.button_toggle("p")),
+    "palate_click": ('light attack release stance', lambda : paired_command_wrapper(lambda : actions.user.mouse_button(0, 16000), lambda : actions.user.button_up("p"))),
+    "oo": ('heavy attack release stance',lambda : paired_command_wrapper(lambda : actions.user.mouse_button_down(2), lambda : actions.user.button_up("p"))),
+}
+
+# This switches the movement directions to be pulses and use the dpad instead of WASD
+menu_actions = {
+    "aa:th_150": ('move left' , lambda : actions.user.button_down("left")),
+    "aa_stop:th_150": ('move left' , lambda : actions.user.button_up("left")),
+    "oh:th_150": ('move right' , lambda : actions.user.button_down("right")),
+    "oh_stop:th_150": ('move right' , lambda : actions.user.button_up("right")),
+    "ee:th_150": ('move up' , lambda : actions.user.button_down("up")),
+    "ee_stop:th_150": ('move up' , lambda : actions.user.button_up("up")),
+    "er:th_150": ('move down' , lambda : actions.user.button_down("down")),
+    "er_stop:th_150": ('move down' , lambda : actions.user.button_up("down")),    
+}
+
+# the word immiscible here is just a silly joke to myself
+# These are actions that I don't know where to fit, So they are only usable while holding the item button
+miscellaneous_immiscible_actions = {
+    "buzz": ('map', lambda : actions.user.button("g")),
+    # This is the same input as default, but does a different action in game so I am labeling it differently
+    "palate_click": ('toggle two hand', lambda : actions.user.mouse_button(0, 16000)), 
+    "zh": ('crouch', lambda : actions.user.button("x")),
+    "tut": ('stance switch', lambda : state_machine_mode_switcher("toggle")),
+}
+
+stance_config = {
+    **default_config,
+    **stance_actions
+}
+
+# This will switch certain noises to use items from the quick menu
+# This will also be used for some miscellaneous commands that I do not want taking up sounds during combat 
+# The quick menu needs a button to be held to access the items therefore the foot switch to access this config
+# will automatically hold that button when the foot switch is being pressed
+item_config = {
+    **default_config,
+    **menu_actions,
+    **miscellaneous_immiscible_actions
+    
+}
+
+menu_config = {
+    **default_config,
+    **menu_actions
+}
+
+parrot_config = {
+    "default": default_config,
+    "stance": stance_config,
+    "item": item_config,
+    "menu": menu_config,
 }
 
 @ctx.action_class("user")
@@ -96,20 +203,29 @@ class EldenringOverrides:
 
     def foot_switch_right_down():
         """Foot switch button right:down"""
-        global parrot_config
+        state_machine_mode_switcher("menu")
             
 
     def foot_switch_right_up(held: bool):
         """Foot switch button right:up"""
-        actions.skip()
+        if actions.user.get_global_variable("stance_active"):
+            state_machine_mode_switcher("stance")
+        else:
+            state_machine_mode_switcher("default")
+            
 
     def foot_switch_top_down():
         """Foot switch button left:down"""
-        actions.skip()
+        state_machine_mode_switcher("item")
+        actions.user.button_down("e")
 
     def foot_switch_top_up(held: bool):
         """Foot switch button left:up"""
-        actions.skip()
+        actions.user.button_up("e")
+        if actions.user.get_global_variable("stance_active"):
+            state_machine_mode_switcher("stance")
+        else:
+            state_machine_mode_switcher("default")
         
         
         
--- gamemode/games/eldenring/eldenring.talon
+++ gamemode/games/eldenring/eldenring.talon
@@ -7,12 +7,16 @@ settings():
     user.game_dpad_mode = "WASD"
     user.dpad_reassert_direction_possible = true
 
-parrot(palate_click_game):
-    user.parrot_config_noise("palate_click")
-
 key(space):
     speech.disable()
     user.sleep_mode_color_preset()
+
+^test eldenring$:
+    print("eldenring working")
+
+
+parrot(palate_click_game):
+    user.parrot_config_noise("palate_click")
 
 parrot(clock):
     user.parrot_config_noise("clock")
@@ -20,26 +24,14 @@ parrot(clock):
 parrot(tut):
     user.parrot_config_noise("tut")
 
-parrot(oo_lenient):
-    user.parrot_config_noise("oo")
+parrot(alveolar_click):
+    user.parrot_config_noise("alveolar_click")
 
-parrot(oo_lenient:stop):
-    user.parrot_config_noise("oo_stop")
+parrot(oh):
+    user.parrot_config_noise("oh")
 
-parrot(buzz):
-    user.parrot_config_noise("buzz")
-
-parrot(er):
-    user.parrot_config_noise("er")
-
-parrot(er:stop):
-    user.parrot_config_noise("er_stop")
-
-parrot(t_lenient):
-    user.parrot_config_noise("t")
-
-parrot(eh):
-    user.parrot_config_noise("eh")
+parrot(oh:stop):
+    user.parrot_config_noise("oh_stop")
 
 parrot(aa):
     user.parrot_config_noise("aa")
@@ -53,17 +45,26 @@ parrot(ee):
 parrot(ee:stop):
     user.parrot_config_noise("ee_stop")
 
-parrot(oh):
-    user.parrot_config_noise("oh")
+parrot(er):
+    user.parrot_config_noise("er")
 
-parrot(oh:stop):
-    user.parrot_config_noise("oh_stop")
+parrot(er:stop):
+    user.parrot_config_noise("er_stop")
 
-parrot(shush):
-    user.parrot_config_noise("shush")
+parrot(oo_lenient):
+    user.parrot_config_noise("oo")
 
-parrot(shush:stop):
-    user.parrot_config_noise("shush_stop")
+parrot(oo_lenient:stop):
+    user.parrot_config_noise("oo_stop")    
+    
+parrot(eh):
+    user.parrot_config_noise("eh")
+
+parrot(ll):
+    user.parrot_config_noise("ll")
+
+parrot(ll:stop):
+    user.parrot_config_noise("ll_stop")
 
 parrot(hiss):
     user.parrot_config_noise("hiss")
@@ -71,11 +72,38 @@ parrot(hiss):
 parrot(hiss:stop):
     user.parrot_config_noise("hiss_stop")
 
+parrot(shush):
+    user.parrot_config_noise("shush")
+
+parrot(shush:stop):
+    user.parrot_config_noise("shush_stop")
+
 parrot(mm):
     user.parrot_config_noise("mm")
 
 parrot(mm:stop):
     user.parrot_config_noise("mm_stop")
 
-^test eldenring$:
-    print("eldenring working")
+parrot(buzz):
+    user.parrot_config_noise("buzz")
+
+parrot(buzz:stop):
+    user.parrot_config_noise("buzz_stop")
+
+parrot(zh):
+    user.parrot_config_noise("zh")
+
+parrot(zh:stop):
+    user.parrot_config_noise("zh_stop")
+
+parrot(t):
+    user.parrot_config_noise("t")
+
+parrot(g):
+    user.parrot_config_noise("g")
+
+parrot(high_whistle):
+    user.parrot_config_noise("high_whistle")
+
+parrot(high_whistle:stop):
+    user.parrot_config_noise("high_whistle_stop")
